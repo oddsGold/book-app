b5cc8d66f6a65d50c0bfd589c87d46d396359409
Sulym Dmytro <ds.intelserv@gmail.com>
2024-05-17 12:36:58 +0300

    book-app/add client dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "reset-css/reset.css";
 import './App.css'
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {selectIsRefreshing} from "./redux/auth/selectors.js";
+import {refreshUser} from "./redux/auth/operations.js";
+import {Layout} from "./components/Layout.jsx";
+import { Route, Routes } from 'react-router-dom';
+import {RestrictedRoute} from "./components/RestrictedRoute.jsx";
+import {PrivateRoute} from "./components/PrivateRoute.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import ContactsPage from "./pages/ContactsPage/ContactsPage.jsx";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const dispatch = useDispatch();
+    const { isRefreshing } = useSelector(selectIsRefreshing);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        dispatch(refreshUser());
+    }, [dispatch]);
+
+    return isRefreshing ? (
+        <b>Refreshing user...</b>
+    ) : (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route
+                    path="/register"
+                    element={
+                        <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+                    }
+                />
+                <Route
+                    path="/contacts"
+                    element={
+                        <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+                    }
+                />
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App
